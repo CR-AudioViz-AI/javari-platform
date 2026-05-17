58a660f707cd2faf180bddc9640aea7839558e3d
Old <225056366+roy-henderson@users.noreply.github.com>
2026-05-16 23:15:31 -0400

    fix3 (#5)
    * fix next.config.js
    
    * fix .npmrc

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 /** @type {import("next").NextConfig} */
-const nextConfig={typescript:{ignoreBuildErrors:true},eslint:{ignoreDuringBuilds:true}}
-module.exports=nextConfig
+const nextConfig = {
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  reactStrictMode: false,
+  // Resolve parallel pages conflict by disabling static optimization
+  experimental: { workerThreads: false, cpus: 1 }
+}
+module.exports = nextConfig
